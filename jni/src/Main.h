51b3805432ff51c12a7a55a152d5c8582de7f84a
EnemySaga <enemysaga1@gmail.com>
2025-06-26 20:27:34 +0700

    Add files via upload

---
+++ jni/src/Main.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#ifndef CHEATBOT_H
+#define CHEATBOT_H
+
+#include "Includes.h"
+
+#include <cstdint>
+
+#include "../types.h"
+
+#include "../Elf/elf.h"
+
+#include "../Logger.h"
+
+
+
+
+using namespace std;
+
+static std::string Media_Folder;
+static char* lib_name = "libUE4.so";
+static uintptr_t UE4 = 0;
+static std::string pkgName = "com.vng.pubgmobile";
+static std::string gameVersion = "2.9";
+static std::string gameShortName = "SANJAY_SRC";
+static std::string gameFullName = "Pubg Mobile";
+
+
+
+
+#endif
